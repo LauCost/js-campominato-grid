ca624fc8e6bb86a95326d8e23913bcf448bd26be
Laurentiu <lcostin40@gmail.com>
2021-10-20 22:24:23 +0200

    Aggiunti commenti per rendere più comprensibile il codice

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,10 @@
-
+//Chiede all'utente di selezionare la difficoltà
 const difficoltà = prompt("Scegli la tua difficoltà da 1 a 3 (1 è Easy!, 2 è Normal!, 3 è Hard!");
 
+//Seleziona l'elemento della DOM sul quale verrà stampata la griglia
 const container = document.querySelector(".cont")
 
+//In base alla difficoltà scelta dall'utente selezionerà la grandezza della griglia
 if (difficoltà == 1) {
 
     var quadrati = 50;
@@ -16,18 +18,25 @@ if (difficoltà == 1) {
     var quadrati = 101;
 }
 
+//Stampa sulla DOM la grandezza della griglia in base alla difficoltà scelta
 for (i = 1; i < quadrati; i++) {
 
+    //Crea un elemento div
     const divEl = document.createElement("div");
 
+    //Aggiunge la classe quadrato all'elemento appena creato
     divEl.className = "quadrato"
 
+    //Stampa la griglia nella DOM
     container.append(divEl)
 
+    //Stampa i numeri dei quadrati nella DOM
     divEl.innerHTML += `<p class="numero"> ${i} </p>`
 
+    //Funzione che server a colorare un quadrato quando ci si clicca sopra
     divEl.addEventListener(`click`, function () {
 
+        //Aggiunge il colore celeste all'elemento cliccato
         this.style.backgroundColor = "cyan"
     })
 }
