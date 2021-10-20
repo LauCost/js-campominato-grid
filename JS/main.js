052245050875427104b7b8030fa27a371c65bbc8
Laurentiu <lcostin40@gmail.com>
2021-10-20 22:06:48 +0200

    Aggiunto codice js che chiede all'utente di selezionare la difficoltà e creare una griglia in base alla difficoltà scelta.

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,22 @@
-const container = document.querySelector(".container")
 
-for (i = 1; i < 50; i++) {
+const difficoltà = prompt("Scegli la tua difficoltà da 1 a 3 (1 è Easy!, 2 è Normal!, 3 è Hard!");
+
+const container = document.querySelector(".cont")
+
+if (difficoltà == 1) {
+
+    var quadrati = 50;
+
+} else if (difficoltà == 2) {
+
+    var quadrati = 82;
+
+} else {
+
+    var quadrati = 101;
+}
+
+for (i = 1; i < quadrati; i++) {
 
     const divEl = document.createElement("div");
 
@@ -8,3 +24,5 @@ for (i = 1; i < 50; i++) {
 
     container.append(divEl)
 }
+
+
